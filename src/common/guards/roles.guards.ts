ff8475185c3a75c677b466f21e7d527bc33539c1
devmarquinhos <marcos.emanuel.04@gmail.com>
2025-10-28 14:36:22 -0300

    :cop: Properly added roles
    The user can be a common user or admin, as admin you have access to critical endpoints

---
+++ src/common/guards/roles.guards.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { user_role } from '@prisma/client';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<user_role[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    const hasPermission = requiredRoles.some((role) => user.role === role);
+
+    if (hasPermission) {
+      return true;
+    } else {
+      throw new ForbiddenException(
+        'Você não tem permissão para acessar este recurso.',
+      );
+    }
+  }
+}
